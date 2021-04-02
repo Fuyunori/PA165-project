b83797f0d96c180c79f93239f41a55928e472058
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-02 23:53:46 +0200

    Set up Spring Boot with Spring Data JPA and H2

---
+++ src/main/java/tennisclub/TennisClubApplication.java
@@ -0,0 +1,11 @@
+package tennisclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisClubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TennisClubApplication.class, args);
+	}
+}
