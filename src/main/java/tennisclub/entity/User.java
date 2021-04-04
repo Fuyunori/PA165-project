2e144ede5af0c13297915c923bc68d3024c0747b
Ondra Holub <ondrejholub202@gmail.com>
2021-04-04 22:39:08 +0200

    Created User entity

--- src/main/java/tennisclub/entity/User.java
+++ src/main/java/tennisclub/entity/User.java
@@ -1,14 +1,82 @@
 package tennisclub.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import tennisclub.entity.ranking.Ranking;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    private String name;
+
+    private  String username;
+
+    private  String passwordHash;
+
+    private String email;
+
+    private Role role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean verifyPasswordHash(String hash) {
+        return hash.equals(passwordHash);
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    enum Role {
+        USER,
+        MANAGER
+    }
+
+    @ManyToMany
+    private Set<Booking> bookings = new HashSet<>();
+
+//    @ManyToMany
+//    private Set<Lesson>  lessonsToTeach = new HashSet<>();
+
+//    @ManyToMany
+//    private Set<Lesson> lessonsToAttend = new HashSet<>();
+
+    @OneToMany
+    private Set<Ranking> rankings = new HashSet<>();
 
 }
