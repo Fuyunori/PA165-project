1d135441013c3b5adcc1ed810367840bf5b9b908
Xuan Linh Phamová <xphamov2@fi.muni.cz>
2021-04-05 17:43:33 +0200

    removed transactional from LessonDAO

--- src/main/java/tennisclub/dao/LessonDaoImpl.java
+++ src/main/java/tennisclub/dao/LessonDaoImpl.java
@@ -9,7 +9,6 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-@Transactional
 public class LessonDaoImpl implements LessonDao{
     @PersistenceContext
     private EntityManager em;
