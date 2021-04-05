4384face0313b625f9f0bfb5feea410038c6b36e
Xuan Linh Phamová <xphamov2@fi.muni.cz>
2021-04-05 17:42:46 +0200

    added Javadoc to LessonDAO interface

--- src/main/java/tennisclub/dao/LessonDao.java
+++ src/main/java/tennisclub/dao/LessonDao.java
@@ -4,13 +4,58 @@ import tennisclub.entity.Lesson;
 
 import java.util.List;
 
+/**
+ * DAO interface for CRUDing Lessons, therefore:
+ * - creating new Lessons
+ * - updating Lessons
+ * - removing Lessons
+ * - retrieving all Lessons.
+ *
+ * Additionally, the interface allows the client to:
+ * - find Lessons by ID,
+ * - find Lessons by capacity.
+ *
+ * @author Xuan Linh Phamová
+ */
 public interface LessonDao {
+    /**
+     * Creates a new lesson.
+     * @param lesson to be created
+     */
     void create(Lesson lesson);
+
+    /**
+     * Updates a lesson.
+     * @param lesson to be updated
+     */
     void update(Lesson lesson);
+
+    /**
+     * Removes a lesson. In case the lesson is in detached state,
+     * it is reattached and then removed.
+     * @param lesson to be removed
+     */
     void remove(Lesson lesson);
 
+    /**
+     * Retrieves all Lessons.
+     * @return all lessons
+     */
     List<Lesson> findAll();
+
+    /**
+     * Finds a particular Lesson by its id.
+     * @param id of the searched lesson
+     * @return a Lesson whose id matches the given id
+     */
     Lesson findById(Long id);
+
     //List<Lesson> findByLecturerName(String lecturerName);
+
+    /**
+     * Finds all Lessons with particular capacity.
+     * @param capacity of the lessons
+     * @return all Lessons which have the given capacity
+     */
     List<Lesson> findByCapacity(Integer capacity);
 }
