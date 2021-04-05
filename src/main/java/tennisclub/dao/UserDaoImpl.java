060713296aa18b404fd31dd545a8763e849d25f6
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-05 23:11:00 +0200

    UserDaoImpl implements UserDao

--- src/main/java/tennisclub/dao/UserDaoImpl.java
+++ src/main/java/tennisclub/dao/UserDaoImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
  * @author Ondrej Holub
  */
 @Repository
-public class UserDaoImpl {
+public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
     EntityManager em;
