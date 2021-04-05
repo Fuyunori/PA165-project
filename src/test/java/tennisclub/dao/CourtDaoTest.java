c60615dd9f29092bc921778f2e145c9afe2df98b
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-05 18:00:37 +0200

    Fix CourtDaoTest to match implementation changes

--- src/test/java/tennisclub/dao/CourtDaoTest.java
+++ src/test/java/tennisclub/dao/CourtDaoTest.java
@@ -3,6 +3,7 @@ package tennisclub.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import tennisclub.entity.Court;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class CourtDaoTest {
 	@Autowired
 	private CourtDao courtDao;
@@ -20,7 +22,7 @@ class CourtDaoTest {
 		court.setAddress("Hell");
 		courtDao.create(court);
 
-		List<Court> courts = courtDao.findByLocation("Hell");
+		List<Court> courts = courtDao.findByAddress("Hell");
 		assertThat(courts.size()).isEqualTo(1);
 		assertThat(courts.get(0).getAddress()).isEqualTo("Hell");
 	}
