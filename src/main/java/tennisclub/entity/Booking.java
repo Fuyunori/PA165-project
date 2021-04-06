9f0038d74743bcae072367fbca579d48d47ed3ed
Miroslav Demek <xdemek@fi.muni.cz>
2021-04-06 23:43:58 +0200

    Add constructor for conveniece

--- src/main/java/tennisclub/entity/Booking.java
+++ src/main/java/tennisclub/entity/Booking.java
@@ -2,6 +2,7 @@ package tennisclub.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,6 +15,12 @@ public class Booking extends Event {
 
     @ManyToMany
     private Set<User> users = new HashSet<>();
+
+    public Booking() { }
+
+    public Booking(LocalDateTime startTime, LocalDateTime endTime) {
+        super(startTime, endTime);
+    }
 
     public Set<User> getUsers() {
         return Collections.unmodifiableSet(users);
