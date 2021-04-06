2a670520ecbcb4f85ba93d3a1e262d4183e41463
Fuyunori <skywingsfs@gmail.com>
2021-04-06 20:47:48 +0200

    added findByXXX methods in LessonDAO interface + implementation
    The added methods are the ones present in the EventDAO interface, therefore:
    - find by court
    - find by start time
    - find by end time
    - find by time interval

--- src/main/java/tennisclub/dao/LessonDaoImpl.java
+++ src/main/java/tennisclub/dao/LessonDaoImpl.java
@@ -1,12 +1,15 @@
 package tennisclub.dao;
 
 import org.springframework.stereotype.Repository;
+import tennisclub.entity.Court;
+import tennisclub.entity.Event;
 import tennisclub.entity.Lesson;
 import tennisclub.entity.enums.Level;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -47,6 +50,35 @@ public class LessonDaoImpl implements LessonDao{
     public List<Lesson> findByLecturerName(String lecturerName) {
         return null;
     }*/
+
+    @Override
+    public List<Lesson> findByCourt(Court court) {
+        return em.createQuery("select l from Lesson l where l.court = :court", Lesson.class)
+                .setParameter("court", court)
+                .getResultList();
+    }
+
+    @Override
+    public List<Lesson> findByStartTime(LocalDateTime startTime) {
+        return em.createQuery("select l from Lesson l where l.startTime = :startTime", Lesson.class)
+                .setParameter("startTime", startTime)
+                .getResultList();
+    }
+
+    @Override
+    public List<Lesson> findByEndTime(LocalDateTime endTime) {
+        return em.createQuery("select l from Lesson l where l.endTime = :endTime", Lesson.class)
+                .setParameter("endTime", endTime)
+                .getResultList();
+    }
+
+    @Override
+    public List<Lesson> findByTimeInterval(LocalDateTime from, LocalDateTime to) {
+        return em.createQuery("select l from Lesson l where l.endTime >= :from and l.startTime <= :to", Lesson.class)
+                .setParameter("from", from)
+                .setParameter("to", to)
+                .getResultList();
+    }
 
     @Override
     public List<Lesson> findByCapacity(Integer capacity) {
