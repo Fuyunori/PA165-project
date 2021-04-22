aa2e119fff09803c316379f7fefc980fc6763c8e
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-22 22:10:51 +0200

    Test CourtService.listByAddress()

--- service/src/test/java/tennisclub/service/CourtServiceTest.java
+++ service/src/test/java/tennisclub/service/CourtServiceTest.java
@@ -6,8 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import tennisclub.dao.CourtDao;
 import tennisclub.entity.Court;
+import java.util.List;
 
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class CourtServiceTest {
@@ -23,5 +27,15 @@ public class CourtServiceTest {
         courtService.create(court);
 
         verify(courtDao).create(court);
+    }
+
+    @Test
+    void listByAddress() {
+        Court court = new Court();
+        when(courtDao.findByAddress("Botanická")).thenReturn(asList(court));
+
+        List<Court> courts = courtService.listByAddress("Botanická");
+        assertThat(courts).hasSize(1);
+        assertThat(courts).contains(court);
     }
 }
