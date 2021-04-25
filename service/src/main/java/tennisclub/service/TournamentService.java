5f73424811a7cb5b0678ed4058ed49f96f56183b
Fuyunori <skywingsfs@gmail.com>
2021-04-25 19:30:17 +0200

    added some methods
    - added some missing findBy methods
    - added a method for adding a ranking

--- service/src/main/java/tennisclub/service/TournamentService.java
+++ service/src/main/java/tennisclub/service/TournamentService.java
@@ -1,6 +1,7 @@
 package tennisclub.service;
 
 import tennisclub.entity.Tournament;
+import tennisclub.entity.ranking.Ranking;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +12,7 @@ public interface TournamentService {
     void remove(Tournament tournament);
     List<Tournament> listByStartTime(LocalDateTime startTime);
     List<Tournament> listByEndTime(LocalDateTime endTime);
+    List<Tournament> listByTimeInterval(LocalDateTime from, LocalDateTime to);
+    List<Tournament> listByCapacity(Integer capacity);
+    void addRanking(Tournament tournament, Ranking ranking);
 }
--- service/src/main/java/tennisclub/service/TournamentServiceImpl.java
+++ service/src/main/java/tennisclub/service/TournamentServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tennisclub.dao.TournamentDao;
 import tennisclub.entity.Tournament;
+import tennisclub.entity.ranking.Ranking;
+import tennisclub.exceptions.TennisClubManagerException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -40,5 +42,25 @@ public class TournamentServiceImpl implements TournamentService {
     @Override
     public List<Tournament> listByEndTime(LocalDateTime endTime) {
         return tournamentDao.findByEndTime(endTime);
+    }
+
+    @Override
+    public List<Tournament> listByTimeInterval(LocalDateTime from, LocalDateTime to) {
+        return tournamentDao.findByTimeInterval(from, to);
+    }
+
+    @Override
+    public List<Tournament> listByCapacity(Integer capacity) {
+        return tournamentDao.findByCapacity(capacity);
+    }
+
+    @Override
+    public void addRanking(Tournament tournament, Ranking ranking){
+        if(tournament.getRankings().contains(ranking)){
+            throw new TennisClubManagerException("Tournament already contains the rank of the player.\n Tournament: "
+                    + tournament.getId() + ",\n player: "
+                    + ranking.getPlayer().getId());
+        }
+        tournament.addRanking(ranking);
     }
 }
