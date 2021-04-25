b31a94b4a1994ce8650e6d256e2ccb9d1884462f
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-26 00:20:40 +0200

    Fix CourtDto not to inherit from CourtCreateDto

--- api/src/main/java/tennisclub/dto/CourtDto.java
+++ api/src/main/java/tennisclub/dto/CourtDto.java
@@ -1,7 +1,13 @@
 package tennisclub.dto;
 
-public class CourtDto extends CourtCreateDto {
+import tennisclub.enums.CourtType;
+
+public class CourtDto {
     private Long id;
+    private String name;
+    private String address;
+    private CourtType type;
+    private String previewImageUrl;
 
     public Long getId() {
         return id;
@@ -9,5 +15,37 @@ public class CourtDto extends CourtCreateDto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public CourtType getType() {
+        return type;
+    }
+
+    public void setType(CourtType type) {
+        this.type = type;
+    }
+
+    public String getPreviewImageUrl() {
+        return previewImageUrl;
+    }
+
+    public void setPreviewImageUrl(String previewImageUrl) {
+        this.previewImageUrl = previewImageUrl;
     }
 }
