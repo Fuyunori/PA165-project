aeafe0be635c329c67e1e6a22f595214132e3f4b
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-25 22:04:13 +0200

    Create straightforward counterparts of CourtDao methods in CourtService

--- service/src/main/java/tennisclub/service/CourtServiceImpl.java
+++ service/src/main/java/tennisclub/service/CourtServiceImpl.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tennisclub.dao.CourtDao;
 import tennisclub.entity.Court;
+import tennisclub.entity.enums.CourtType;
 import java.util.List;
 
+/**
+ * Implementation of {@link CourtService}
+ * @author Pavel Tobiáš
+ */
 @Service
 public class CourtServiceImpl implements CourtService {
     private final CourtDao courtDao;
@@ -21,7 +26,27 @@ public class CourtServiceImpl implements CourtService {
     }
 
     @Override
+    public Court update(Court court) {
+        return courtDao.update(court);
+    }
+
+    @Override
+    public void delete(Court court) {
+        courtDao.delete(court);
+    }
+
+    @Override
+    public Court getById(Long id) {
+        return courtDao.findById(id);
+    }
+
+    @Override
     public List<Court> listByAddress(String address) {
         return courtDao.findByAddress(address);
+    }
+
+    @Override
+    public List<Court> listByType(CourtType type) {
+        return courtDao.findByType(type);
     }
 }
