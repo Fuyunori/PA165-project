3d510877667911b5cafa9764cda19b32d8c0aa9a
Miroslav Demek <xdemek@fi.muni.cz>
2021-04-26 23:13:03 +0200

    Create basic BookingService interface and implementation

---
+++ service/src/main/java/tennisclub/service/BookingService.java
@@ -0,0 +1,31 @@
+package tennisclub.service;
+
+import tennisclub.entity.Booking;
+import tennisclub.entity.Court;
+import tennisclub.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BookingService {
+
+    void createBooking(Booking booking);
+
+    Booking updateBooking(Booking booking);
+
+    void deleteBooking(Booking booking);
+
+    void removeUser(Booking booking, User user);
+
+    void addUser(Booking booking, User user);
+
+    List<Booking> findALL();
+
+    Booking findById(Long id);
+
+    List<Booking> findByUser(User user);
+
+    List<Booking> findByCourt(Court court);
+
+    List<Booking> findByTimeInterval(LocalDateTime from, LocalDateTime to);
+}
---
+++ service/src/main/java/tennisclub/service/BookingServiceImpl.java
@@ -0,0 +1,79 @@
+package tennisclub.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tennisclub.dao.BookingDao;
+import tennisclub.entity.Booking;
+import tennisclub.entity.Court;
+import tennisclub.entity.User;
+import tennisclub.exceptions.TennisClubManagerException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class BookingServiceImpl implements BookingService {
+
+    final private BookingDao bookingDao;
+
+    @Autowired
+    public BookingServiceImpl(BookingDao bookingDao) {
+        this.bookingDao = bookingDao;
+    }
+
+    @Override
+    public void createBooking(Booking booking) {
+        bookingDao.create(booking);
+    }
+
+    @Override
+    public Booking updateBooking(Booking booking) {
+        return bookingDao.update(booking);
+    }
+
+    @Override
+    public void deleteBooking(Booking booking) {
+        bookingDao.delete(booking);
+    }
+
+    @Override
+    public void removeUser(Booking booking, User user) {
+        if (!booking.getUsers().contains(user)) {
+            throw new TennisClubManagerException("Removing user from a booking he is not in!");
+        }
+        booking.removeUser(user);
+    }
+
+    @Override
+    public void addUser(Booking booking, User user) {
+        if (booking.getUsers().contains(user)) {
+            throw new TennisClubManagerException("Adding user to a booking he is already in!");
+        }
+        booking.addUser(user);
+    }
+
+    @Override
+    public List<Booking> findALL() {
+        return bookingDao.findAll();
+    }
+
+    @Override
+    public Booking findById(Long id) {
+        return bookingDao.findById(id);
+    }
+
+    @Override
+    public List<Booking> findByUser(User user) {
+        return bookingDao.findByUser(user);
+    }
+
+    @Override
+    public List<Booking> findByCourt(Court court) {
+        return bookingDao.findByCourt(court);
+    }
+
+    @Override
+    public List<Booking> findByTimeInterval(LocalDateTime from, LocalDateTime to) {
+        return bookingDao.findByTimeInterval(from, to);
+    }
+}
