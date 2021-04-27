02f0e6117667ec62652ae2450c6865aab990e920
Fuyunori <skywingsfs@gmail.com>
2021-04-27 17:44:46 +0200

    moved methods in the service due to consistency

--- service/src/main/java/tennisclub/service/LessonService.java
+++ service/src/main/java/tennisclub/service/LessonService.java
@@ -13,7 +13,6 @@ public interface LessonService {
     void create(Lesson lesson);
     Lesson update(Lesson lesson);
     void remove(Lesson lesson);
-    Lesson listById(Long id);
 
     void enrollStudent(Lesson lesson, User student);
     void addTeacher(Lesson lesson, User teacher);
@@ -21,6 +20,7 @@ public interface LessonService {
     void withdrawStudent(Lesson lesson, User student);
     void removeTeacher(Lesson lesson, User teacher);
 
+    Lesson listById(Long id);
     List<Lesson> listAll();
     List<Lesson> listByStartTime(LocalDateTime startTime);
     List<Lesson> listByEndTime(LocalDateTime endTime);
