e02d5d70dd9321e5be80faa515352efd66b73385
Ondra Holub <ondrejholub202@gmail.com>
2021-04-27 13:54:06 +0200

    Added basic CRUD RankingService methods

--- service/src/main/java/tennisclub/service/RankingService.java
+++ service/src/main/java/tennisclub/service/RankingService.java
@@ -1,4 +1,16 @@
 package tennisclub.service;
 
+import tennisclub.entity.Tournament;
+import tennisclub.entity.User;
+import tennisclub.entity.ranking.Ranking;
+
+import java.util.List;
+
 public interface RankingService {
+    void create(Ranking ranking);
+    Ranking find(User player, Tournament tournament);
+    List<Ranking> findByPlayer(User player);
+    List<Ranking> findByTournament(Tournament tournament);
+    void update(Ranking ranking);
+    void remove(Ranking ranking);
 }
