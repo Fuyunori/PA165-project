f3801abe35f4c4cd2f9eec8c3daa62419818a454
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-29 22:47:26 +0200

    Test UserService.register()

---
+++ service/src/test/java/tennisclub/service/UserServiceTest.java
@@ -0,0 +1,32 @@
+package tennisclub.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tennisclub.dao.UserDao;
+import tennisclub.entity.User;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Pavel Tobiáš
+ */
+@SpringBootTest
+public class UserServiceTest {
+    @MockBean
+    private UserDao userDao;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void register() {
+        User user = new User();
+        userService.register(user, "password");
+
+        verify(userDao).create(user);
+        assertThat(user.getPasswordHash()).isNotBlank();
+    }
+}
