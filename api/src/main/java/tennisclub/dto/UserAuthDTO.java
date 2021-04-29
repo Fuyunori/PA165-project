1db27fb1116bd43d2b788299875d5b725b35ade9
Ondra Holub <ondrejholub202@gmail.com>
2021-04-29 12:35:23 +0200

    Added UserAuthDTO

---
+++ api/src/main/java/tennisclub/dto/UserAuthDTO.java
@@ -0,0 +1,25 @@
+package tennisclub.dto;
+
+public class UserAuthDTO {
+
+    private  String username;
+
+    private  String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return password;
+    }
+
+    public void setPasswordHash(String password) {
+        this.password = password;
+    }
+}
