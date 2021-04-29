dabed8ca1a99e25bfda2903de148220bbdf5e874
Ondra Holub <ondrejholub202@gmail.com>
2021-04-29 12:30:52 +0200

    Added UserDTO

---
+++ api/src/main/java/tennisclub/dto/UserDTO.java
@@ -0,0 +1,57 @@
+package tennisclub.dto;
+
+import tennisclub.enums.Role;
+
+public class UserDTO {
+
+    private Long id;
+
+    private String name;
+
+    private  String username;
+
+    private  String passwordHash;
+
+    private String email;
+
+    private Role role;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPasswordHash() { return passwordHash; }
+
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public Role getRole() { return role; }
+
+    public void setRole(Role role) { this.role = role; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDTO)) return false;
+
+        UserDTO userDTO = (UserDTO) o;
+
+        return getUsername() != null ? getUsername().equals(userDTO.getUsername()) : userDTO.getUsername() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getUsername() != null ? getUsername().hashCode() : 0;
+    }
+}
