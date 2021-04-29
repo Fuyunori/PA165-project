587403ee7a885ce444c6e0d44460da9d6982244d
Ondra Holub <ondrejholub202@gmail.com>
2021-04-29 14:32:04 +0200

    Added validation to UserAuthDTO (username and password not null)

--- api/src/main/java/tennisclub/dto/UserAuthDTO.java
+++ api/src/main/java/tennisclub/dto/UserAuthDTO.java
@@ -1,9 +1,13 @@
 package tennisclub.dto;
 
+import javax.validation.constraints.NotNull;
+
 public class UserAuthDTO {
 
+    @NotNull
     private  String username;
 
+    @NotNull
     private  String password;
 
 
