1b38192fb565baf461ad5eff5e7ee9e2436f4945
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-30 01:01:58 +0200

    Test UserService.findByUserId()

--- service/src/test/java/tennisclub/service/UserServiceTest.java
+++ service/src/test/java/tennisclub/service/UserServiceTest.java
@@ -35,12 +35,18 @@ public class UserServiceTest {
     @BeforeEach
     void initUsers() {
         user = new User();
+        user.setId(1L);
+        user.setUsername("destroyer777");
         user.setRole(Role.USER);
 
         otherUser = new User();
+        otherUser.setUsername("notJohn");
+        otherUser.setId(2L);
         otherUser.setRole(Role.USER);
 
         manager = new User();
+        manager.setUsername("tennis_hustler");
+        manager.setId(3L);
         manager.setRole(Role.MANAGER);
 
         password = "Never.gonna_give-You up!";
@@ -82,5 +88,32 @@ public class UserServiceTest {
         verify(userDao).findAll();
         assertThat(users).hasSize(3);
         assertThat(users).contains(user, otherUser, manager);
+    }
+
+    @Test
+    void findByUserId() {
+        when(userDao.findById(1L)).thenReturn(user);
+        when(userDao.findById(2L)).thenReturn(otherUser);
+        when(userDao.findById(3L)).thenReturn(manager);
+        when(userDao.findById(42L)).thenReturn(null);
+
+        User foundUser;
+
+        foundUser = userService.findUserById(1L);
+        verify(userDao).findById(1L);
+        assertThat(foundUser).isEqualTo(user);
+
+        foundUser = userService.findUserById(2L);
+        verify(userDao).findById(2L);
+        assertThat(foundUser).isEqualTo(otherUser);
+
+        foundUser = userService.findUserById(3L);
+        verify(userDao).findById(3L);
+        assertThat(foundUser).isEqualTo(manager);
+
+        // TODO is this good practice? Should an exception be thrown instead?
+        foundUser = userService.findUserById(42L);
+        verify(userDao).findById(42L);
+        assertThat(foundUser).isNull();
     }
 }
