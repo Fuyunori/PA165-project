b714ebe6df388778ddfcda48a6216126c18c8b22
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-30 20:33:54 +0200

    Test UserFacade.register()

--- service/src/test/java/tennisclub/facade/UserFacadeTest.java
+++ service/src/test/java/tennisclub/facade/UserFacadeTest.java
@@ -1,10 +1,19 @@
 package tennisclub.facade;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import tennisclub.dto.user.UserAuthDTO;
+import tennisclub.dto.user.UserDTO;
+import tennisclub.entity.User;
 import tennisclub.service.UserService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UserFacadeTest {
@@ -14,9 +23,31 @@ public class UserFacadeTest {
     @Autowired
     private UserFacade userFacade;
 
+    private UserAuthDTO authDto;
+    private User authEntity;
+
+    @BeforeEach
+    void init() {
+        authDto = new UserAuthDTO();
+        authDto.setUsername("us3rn4m3");
+        authDto.setPassword("p4ssw0rd");
+
+        authEntity = new User();
+        authEntity.setUsername(authDto.getUsername());
+    }
+
     @Test
     void register() {
-        // TODO
+        String username = authDto.getUsername();
+        String password = authDto.getPassword();
+
+        ArgumentCaptor<User> passedEntity = ArgumentCaptor.forClass(User.class);
+        ArgumentCaptor<String> passedPassword = ArgumentCaptor.forClass(String.class);
+
+        userFacade.register(authDto);
+        verify(userService).register(passedEntity.capture(), passedPassword.capture());
+        assertThat(passedEntity.getValue().getUsername()).isEqualTo(username);
+        assertThat(passedPassword.getValue()).isEqualTo(password);
     }
 
     @Test
