ba611b75920bb4ac9ebbd7edd1f91a8205502a61
Miroslav Demek <xdemek@fi.muni.cz>
2021-04-30 10:46:59 +0200

    Add service annotation to time service

--- service/src/main/java/tennisclub/service/TimeServiceImpl.java
+++ service/src/main/java/tennisclub/service/TimeServiceImpl.java
@@ -1,10 +1,13 @@
 package tennisclub.service;
 
+import org.springframework.stereotype.Service;
+
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Service
 public class TimeServiceImpl implements TimeService {
 
     @Override
