123dcf79258d5802787daa9355e626ffb8f1cbe6
Ondra Holub <ondrejholub202@gmail.com>
2021-04-30 16:49:17 +0200

    Added UserFacadeImpl

---
+++ service/src/main/java/tennisclub/facade/UserFacadeImpl.java
@@ -0,0 +1,100 @@
+package tennisclub.facade;
+
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tennisclub.dto.court.CourtDto;
+import tennisclub.dto.user.UserAuthDTO;
+import tennisclub.dto.user.UserDTO;
+import tennisclub.dto.user.UserFullDTO;
+import tennisclub.entity.User;
+import tennisclub.enums.Role;
+import tennisclub.service.UserService;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class UserFacadeImpl implements UserFacade {
+
+    private final Mapper mapper;
+    private final UserService userService;
+
+    @Autowired
+    public UserFacadeImpl(Mapper mapper, UserService userService) {
+        this.mapper = mapper;
+        this.userService = userService;
+    }
+
+    @Override
+    public void register(UserAuthDTO userAuthDTO, String plainTextPassword) {
+        User user = mapper.map(userAuthDTO, User.class);
+        userService.register(user, plainTextPassword);
+        //TODO now there is no hash in the user entity :/
+        //either userService.register will return the user (or ID), or there will be userService.find*
+        //consider adding plainTextPassword to DTO
+    }
+
+    @Override
+    public boolean authenticate(UserAuthDTO userAuthDTO, String plainTextPassword) {
+        User user = mapper.map(userAuthDTO, User.class);
+        return userService.authenticate(user, plainTextPassword);
+    }
+
+    @Override
+    public boolean hasRights(UserDTO userDTO, Role role) {
+        User user = mapper.map(userDTO, User.class);
+        return userService.hasRights(user, role);
+    }
+
+    @Override
+    public List<UserFullDTO> findAllUsers() {
+        List<User> foundUsers = userService.getAllUsers();
+        return foundUsers.stream()
+                .map(u -> mapper.map(u, UserFullDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserFullDTO findUserById(Long userId) {
+        User foundUser = userService.findUserById(userId);
+        return mapper.map(foundUser, UserFullDTO.class);
+    }
+
+    @Override
+    public List<UserFullDTO> findUsersByName(String name) {
+        List<User> foundUsers = userService.findUsersByName(name);
+        return foundUsers.stream()
+                .map(u -> mapper.map(u, UserFullDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserFullDTO findUserByUsername(String username) {
+        User foundUser = userService.findUserByUsername(username);
+        return mapper.map(foundUser, UserFullDTO.class);
+    }
+
+    @Override
+    public List<UserFullDTO> findUsersByEmail(String email) {
+        List<User> foundUsers = userService.findUsersByEmail(email);
+        return foundUsers.stream()
+                .map(u -> mapper.map(u, UserFullDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserFullDTO updateUser(UserFullDTO userFullDTO) {
+        User user = mapper.map(userFullDTO, User.class);
+        user = userService.updateUserData(user);
+        return mapper.map(user, UserFullDTO.class);
+    }
+
+    @Override
+    public void removeUser(UserDTO userDTO) {
+        User user = mapper.map(userDTO, User.class);
+        userService.removeUser(user);
+    }
+}
