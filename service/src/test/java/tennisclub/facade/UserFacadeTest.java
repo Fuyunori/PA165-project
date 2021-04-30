24d22ef0f68a1a782c2611957ff9c547ea0b042c
Pavel Tobias <pavel.tobias@flowup.cz>
2021-04-30 21:26:59 +0200

    Test UserFacade.hasRights()

--- service/src/test/java/tennisclub/facade/UserFacadeTest.java
+++ service/src/test/java/tennisclub/facade/UserFacadeTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import tennisclub.dto.user.UserAuthDTO;
 import tennisclub.dto.user.UserDTO;
 import tennisclub.entity.User;
+import tennisclub.enums.Role;
 import tennisclub.service.UserService;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,6 +28,8 @@ public class UserFacadeTest {
     private String password;
     private UserAuthDTO authDto;
     private User authEntity;
+    private UserDTO dto;
+    private User entity;
 
     @BeforeEach
     void init() {
@@ -39,6 +42,12 @@ public class UserFacadeTest {
 
         authEntity = new User();
         authEntity.setUsername(authDto.getUsername());
+
+        dto = new UserDTO();
+        dto.setUsername(username);
+
+        entity = new User();
+        entity.setUsername(username);
     }
 
     @Test
@@ -72,7 +81,17 @@ public class UserFacadeTest {
 
     @Test
     void hasRights() {
-        // TODO
+        when(userService.hasRights(entity, Role.USER)).thenReturn(true);
+        assertThat(userFacade.hasRights(dto, Role.USER)).isTrue();
+
+        when(userService.hasRights(entity, Role.USER)).thenReturn(false);
+        assertThat(userFacade.hasRights(dto, Role.USER)).isFalse();
+
+        when(userService.hasRights(entity, Role.MANAGER)).thenReturn(true);
+        assertThat(userFacade.hasRights(dto, Role.MANAGER)).isTrue();
+
+        when(userService.hasRights(entity, Role.MANAGER)).thenReturn(false);
+        assertThat(userFacade.hasRights(dto, Role.MANAGER)).isFalse();
     }
 
     @Test
