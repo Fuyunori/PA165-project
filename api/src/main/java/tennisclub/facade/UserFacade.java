98833d5dc478d8bcc40a6ee684df51463ee369df
Ondra Holub <ondrejholub202@gmail.com>
2021-04-30 22:46:52 +0200

    Added javadoc to UserFacade

--- api/src/main/java/tennisclub/facade/UserFacade.java
+++ api/src/main/java/tennisclub/facade/UserFacade.java
@@ -7,25 +7,76 @@ import tennisclub.enums.Role;
 
 import java.util.List;
 
+/**
+ * Facade for manipulation with users
+ * @author Ondrej Holub
+ */
 public interface UserFacade {
 
+    /**
+     * Registers a new user
+     * @param userAuthDTO containing the user authentication data
+     */
     void register(UserAuthDTO userAuthDTO);
 
+    /**
+     * Authenticates user
+     * @param userAuthDTO containing the user authentication data
+     */
     boolean authenticate(UserAuthDTO userAuthDTO);
 
+    /**
+     * Verifies whether the user has certain rights
+     * @param userDTO containing the user data
+     * @param role the user should have
+     * @return whether the user was authorised
+     */
     boolean hasRights(UserDTO userDTO, Role role);
 
+    /**
+     * Finds all users in the system
+     * @return List of Users
+     */
     List<UserFullDTO> findAllUsers();
 
+    /**
+     * Finds a user based on id
+     * @param userId of the user
+     * @return the user
+     */
     UserFullDTO findUserById(Long userId);
 
+    /**
+     * Finds a user based on name
+     * @param name of the user
+     * @return the user
+     */
     List<UserFullDTO> findUsersByName(String name);
 
+    /**
+     * Finds a user based on username
+     * @param username of the user
+     * @return the user
+     */
     UserFullDTO findUserByUsername(String username);
 
+    /**
+     * Finds a user based on email
+     * @param email of the user
+     * @return the user
+     */
     List<UserFullDTO> findUsersByEmail(String email);
 
+    /**
+     * Updated the data of a user
+     * @param userFullDTO containing the new user data
+     * @return the updated user
+     */
     UserFullDTO updateUser(UserFullDTO userFullDTO);
 
+    /**
+     * Removes a user from the system
+     * @param userDTO to be removed
+     */
     void removeUser(UserDTO userDTO);
 }
