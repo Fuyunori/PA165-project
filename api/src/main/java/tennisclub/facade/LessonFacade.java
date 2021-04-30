d7da0725abadec66cde7ae6300ac6731d3ceced0
Ondra Holub <ondrejholub202@gmail.com>
2021-04-30 23:49:40 +0200

    Added javadoc for all the lessons

--- api/src/main/java/tennisclub/facade/LessonFacade.java
+++ api/src/main/java/tennisclub/facade/LessonFacade.java
@@ -8,20 +8,91 @@ import tennisclub.enums.Level;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Facade for manipulation with lessons
+ * @author Xuan Linh Phamová
+ */
 public interface LessonFacade {
+
+    /**
+     * Creates a new lesson
+     * @param lessonDTO containing data for lesson creation
+     * @return id of the lesson
+     */
     Long createLesson(LessonCreateDTO lessonDTO);
+
+    /**
+     * Deletes a lesson
+     * @param lessonId of the lesson to be deleted
+     */
     void deleteLesson(Long lessonId);
 
+    /**
+     * Enrolls a student into a lesson
+     * @param lessonId of the lesson
+     * @param studentId of the student
+     */
     void enrollStudent(Long lessonId, Long studentId);
+
+    /**
+     * Adds a teacher to a lesson
+     * @param lessonId of the lesson
+     * @param teacherId of the teacher
+     */
     void addTeacher(Long lessonId, Long teacherId);
 
+    /**
+     * Removes a student from a lesson
+     * @param lessonId of the lesson
+     * @param studentId of the student
+     */
     void withdrawStudent(Long lessonId, Long studentId);
+
+    /**
+     * Removes a teacher from a lesson
+     * @param lessonId of the lesson
+     * @param teacherId of the teacher
+     */
     void removeTeacher(Long lessonId, Long teacherId);
 
+    /**
+     * Finds a lesson based on the id
+     * @param id of the lesson
+     * @return the lesson DTO
+     */
     LessonFullDTO getLessonWithId(Long id);
+
+    /**
+     * Finds all lessons
+     * @return List of the lesson DTOs
+     */
     List<LessonFullDTO> getAllLessons();
+
+    /**
+     * Finds lessons based on a start time
+     * @param startTime of the lessons
+     * @return list of the lesson DTOs
+     */
     List<LessonFullDTO> getLessonsByStartTime(LocalDateTime startTime);
+
+    /**
+     * Finds lessons based on an end time
+     * @param endTime of the lessons
+     * @return list of the lesson DTOs
+     */
     List<LessonFullDTO> getLessonsByEndTime(LocalDateTime endTime);
+
+    /**
+     * Finds lessons based on a level
+     * @param level of the lessons
+     * @return list of the lesson DTOs
+     */
     List<LessonFullDTO> getLessonsByLevel(Level level);
+
+    /**
+     * Finds lessons based on a court
+     * @param courtId of the court
+     * @return list of the lesson DTOs
+     */
     List<LessonFullDTO> getLessonsByCourt(Long courtId);
 }
