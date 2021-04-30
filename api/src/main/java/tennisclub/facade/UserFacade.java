4c8baec194ea19a735b128d0944ae40ec8887e91
Ondra Holub <ondrejholub202@gmail.com>
2021-04-30 16:48:31 +0200

    Added UserFacade

---
+++ api/src/main/java/tennisclub/facade/UserFacade.java
@@ -0,0 +1,31 @@
+package tennisclub.facade;
+
+import tennisclub.dto.user.UserAuthDTO;
+import tennisclub.dto.user.UserDTO;
+import tennisclub.dto.user.UserFullDTO;
+import tennisclub.enums.Role;
+
+import java.util.List;
+
+public interface UserFacade {
+
+    void register(UserAuthDTO userAuthDTO, String plainTextPassword);
+
+    boolean authenticate(UserAuthDTO userAuthDTO, String plainTextPassword);
+
+    boolean hasRights(UserDTO userDTO, Role role);
+
+    List<UserFullDTO> findAllUsers();
+
+    UserFullDTO findUserById(Long userId);
+
+    List<UserFullDTO> findUsersByName(String name);
+
+    UserFullDTO findUserByUsername(String username);
+
+    List<UserFullDTO> findUsersByEmail(String email);
+
+    UserFullDTO updateUser(UserFullDTO userFullDTO);
+
+    void removeUser(UserDTO userDTO);
+}
