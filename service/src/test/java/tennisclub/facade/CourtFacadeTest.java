78ce7d4f8c43caf72450b216b546e725616c3b45
Ondra Holub <ondrejholub202@gmail.com>
2021-04-30 21:59:52 +0200

    Added test for CourtFacade.update()

--- service/src/test/java/tennisclub/facade/CourtFacadeTest.java
+++ service/src/test/java/tennisclub/facade/CourtFacadeTest.java
@@ -5,13 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import tennisclub.dto.court.CourtCreateDto;
+import tennisclub.dto.court.CourtDto;
 import tennisclub.entity.Court;
 import tennisclub.enums.CourtType;
 import tennisclub.service.CourtService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class CourtFacadeTest {
@@ -36,5 +39,28 @@ public class CourtFacadeTest {
         expectedEntity.setType(CourtType.TURF);
         expectedEntity.setAddress("Abbey Road");
         verify(courtService).create(refEq(expectedEntity));
+    }
+
+    @Test
+    void update() {
+        CourtDto passedDto = new CourtDto();
+        passedDto.setId(42L);
+        passedDto.setName("Hello");
+        passedDto.setPreviewImageUrl("http://localhost/image.png");
+        passedDto.setType(CourtType.TURF);
+        passedDto.setAddress("Abbey Road");
+
+        Court expectedEntity = new Court();
+        expectedEntity.setId(42L);
+        expectedEntity.setName("Hello");
+        expectedEntity.setPreviewImageUrl("http://localhost/image.png");
+        expectedEntity.setType(CourtType.TURF);
+        expectedEntity.setAddress("Abbey Road");
+
+        when(courtService.update(expectedEntity)).thenReturn(expectedEntity);
+
+        CourtDto result = courtFacade.update(passedDto);
+        verify(courtService).update(refEq(expectedEntity));
+        assertThat(result).isEqualTo(passedDto);
     }
 }
