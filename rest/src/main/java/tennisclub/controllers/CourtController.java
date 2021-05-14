66cbb01884cf4ded8fa3814bf95e9758383ca52f
Pavel Tobias <pavel.tobias@flowup.cz>
2021-05-14 17:50:58 +0200

    Implement basic CourtController

---
+++ rest/src/main/java/tennisclub/controllers/CourtController.java
@@ -0,0 +1,44 @@
+package tennisclub.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tennisclub.dto.court.CourtCreateDto;
+import tennisclub.dto.court.CourtDto;
+import tennisclub.dto.court.CourtUpdateDto;
+import tennisclub.facade.CourtFacade;
+
+import java.util.List;
+
+/**
+ * @author Pavel Tobiáš
+ */
+@RestController
+@RequestMapping("/courts")
+public class CourtController {
+    private final CourtFacade courtFacade;
+
+    @Autowired
+    public CourtController(CourtFacade courtFacade) {
+        this.courtFacade = courtFacade;
+    }
+
+    @GetMapping
+    public List<CourtDto> getCourts() {
+        return courtFacade.listAll();
+    }
+
+    @PostMapping
+    public CourtDto getCourts(@RequestBody CourtCreateDto dto) {
+        return courtFacade.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    public CourtDto putCourt(@PathVariable Long id, @RequestBody CourtUpdateDto dto) {
+        return courtFacade.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourt(@PathVariable Long id) {
+        courtFacade.delete(id);
+    }
+}
