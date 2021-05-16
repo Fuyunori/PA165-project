488e005fe72d28fba71b856dcbaa505b4a8ce672
Fuyunori <skywingsfs@gmail.com>
2021-05-16 14:57:58 +0200

    created a model

---
+++ ui/src/app/models/lesson.model.ts
@@ -0,0 +1,15 @@
+import {Event} from "./event.model";
+import {User} from "./user.model";
+
+export enum Level {
+    Beginner = 'BEGINNER',
+    Intermediate = 'INTERMEDIATE',
+    Advanced = 'ADVANCED'
+}
+
+export type Lesson = Event & {
+    capacity: number;
+    level: Level;
+    teachers: Set<User>;
+    students: Set<User>;
+}
