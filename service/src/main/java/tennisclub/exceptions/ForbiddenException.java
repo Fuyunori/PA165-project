8ee4d68db0d2dbbd3c977f99b845737ebc59e544
Ondra Holub <ondrejholub202@gmail.com>
2021-05-19 23:35:24 +0200

    Created UnauthorisedException and ForbiddenException to allow correct HTTP codes for authentication and authorization

---
+++ service/src/main/java/tennisclub/exceptions/ForbiddenException.java
@@ -0,0 +1,7 @@
+package tennisclub.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException{}
---
+++ service/src/main/java/tennisclub/exceptions/UnauthorisedException.java
@@ -0,0 +1,7 @@
+package tennisclub.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+public class UnauthorisedException extends RuntimeException {}
