ab509a37a895e3391fdd01cae6ecc9c33a8dbfb3
Miroslav Demek <xdemek@fi.muni.cz>
2021-05-19 20:19:44 +0200

    Add simple exception handler

---
+++ rest/src/main/java/tennisclub/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package tennisclub.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tennisclub.exceptions.ServiceLayerException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        FieldError error = ex.getFieldError();
+        if (error != null) {
+            return ResponseEntity.badRequest().body(error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body("Some error.");
+    }
+
+    @ExceptionHandler
+    ResponseEntity<String> handleServiceLayerException(ServiceLayerException ex) {
+        return ResponseEntity.badRequest().body("Internal error.");
+    }
+}
