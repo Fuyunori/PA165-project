f72271432b953eb00f4af5fb53670071534d2afa
Fuyunori <skywingsfs@gmail.com>
2021-05-19 19:44:53 +0200

    added bean validation annotation + added response entity

--- rest/src/main/java/tennisclub/controllers/LessonController.java
+++ rest/src/main/java/tennisclub/controllers/LessonController.java
@@ -1,6 +1,8 @@
 package tennisclub.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tennisclub.dto.lesson.LessonCreateDTO;
 import tennisclub.dto.lesson.LessonFullDTO;
@@ -8,6 +10,7 @@ import tennisclub.dto.user.UserDTO;
 import tennisclub.dto.user.UserFullDTO;
 import tennisclub.facade.LessonFacade;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -25,42 +28,43 @@ public class LessonController {
     }
 
     @GetMapping
-    public List<LessonFullDTO> getLessons(){
-        return lessonFacade.getAllLessons();
+    public ResponseEntity<List<LessonFullDTO>> getLessons(){
+        return ResponseEntity.ok(lessonFacade.getAllLessons());
     }
 
     @GetMapping("/{id}")
-    public LessonFullDTO getLessonById(@PathVariable Long id){
-        return lessonFacade.getLessonWithId(id);
+    public ResponseEntity<LessonFullDTO> getLessonById(@PathVariable Long id){
+        return ResponseEntity.ok(lessonFacade.getLessonWithId(id));
     }
 
     @PostMapping
-    public LessonFullDTO createLesson(@RequestBody LessonCreateDTO lessonDTO){
-        return lessonFacade.createLesson(lessonDTO);
+    public ResponseEntity<LessonFullDTO> createLesson(@Valid @RequestBody LessonCreateDTO lessonDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(lessonFacade.createLesson(lessonDTO));
     }
 
     @PostMapping("/{lessonId}/students")
-    public LessonFullDTO enrollStudent(@PathVariable Long lessonId, @RequestBody UserFullDTO player){
-        return lessonFacade.enrollStudent(lessonId, player.getId());
+    public ResponseEntity<LessonFullDTO> enrollStudent(@PathVariable Long lessonId, @Valid @RequestBody UserFullDTO player){
+        return ResponseEntity.ok(lessonFacade.enrollStudent(lessonId, player.getId()));
     }
 
     @PostMapping("/{lessonId}/teachers")
-    public LessonFullDTO addTeacher(@PathVariable Long lessonId, @RequestBody UserFullDTO teacher){
-        return lessonFacade.addTeacher(lessonId, teacher.getId());
+    public ResponseEntity<LessonFullDTO> addTeacher(@PathVariable Long lessonId, @RequestBody UserFullDTO teacher){
+        return ResponseEntity.ok(lessonFacade.addTeacher(lessonId, teacher.getId()));
     }
 
     @DeleteMapping("/{lessonId}/students/{studentId}")
-    public LessonFullDTO withdrawStudent(@PathVariable Long lessonId, @PathVariable Long studentId){
-        return lessonFacade.withdrawStudent(lessonId, studentId);
+    public ResponseEntity<LessonFullDTO> withdrawStudent(@PathVariable Long lessonId, @PathVariable Long studentId){
+        return ResponseEntity.ok(lessonFacade.withdrawStudent(lessonId, studentId));
     }
 
     @DeleteMapping("/{lessonId}/teachers/{teacherId}")
-    public LessonFullDTO removeTeacher(@PathVariable Long lessonId, @PathVariable Long teacherId){
-        return lessonFacade.removeTeacher(lessonId, teacherId);
+    public ResponseEntity<LessonFullDTO> removeTeacher(@PathVariable Long lessonId, @PathVariable Long teacherId){
+        return ResponseEntity.ok(lessonFacade.removeTeacher(lessonId, teacherId));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteLesson(@PathVariable Long id){
+    public ResponseEntity<Void> deleteLesson(@PathVariable Long id){
         lessonFacade.deleteLesson(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
