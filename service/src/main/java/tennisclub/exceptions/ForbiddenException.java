7ca5177d2af07806272d1fba8072a2b21c6236b8
Ondra Holub <ondrejholub202@gmail.com>
2021-05-19 23:39:57 +0200

    Added javadoc

--- service/src/main/java/tennisclub/exceptions/ForbiddenException.java
+++ service/src/main/java/tennisclub/exceptions/ForbiddenException.java
@@ -3,5 +3,9 @@ package tennisclub.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception resulting in HTTP code 403
+ * @author Ondrej Holub
+ */
 @ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException{}
