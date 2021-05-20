206884f476ae9b8ee7907a7e677a3cd20321b059
Pavel Tobias <pavel.tobias@flowup.cz>
2021-05-21 00:58:43 +0200

    Display table of events in court detail view

--- ui/src/app/views/main/court-detail/court-detail.component.ts
+++ ui/src/app/views/main/court-detail/court-detail.component.ts
@@ -5,6 +5,13 @@ import { takeUntil } from 'rxjs/operators';
 import { Court, UnknownCourt } from '../../../models/court.model';
 import { AuthService } from '../../../services/auth.service';
 import { CourtService } from '../../../services/court.service';
+import { EventService } from '../../../services/event.service';
+import { Event } from '../../../models/event.model';
+
+enum EventTableColumn {
+  StartTime = 'StartTime',
+  EndTime = 'EndTime',
+}
 
 @Component({
   selector: 'tc-court-detail',
@@ -13,8 +20,15 @@ import { CourtService } from '../../../services/court.service';
 })
 export class CourtDetailComponent implements OnInit, OnDestroy {
   displayedCourt$: Observable<Court | null> = of(null);
+  displayedEvents$: Observable<Event[]> = of([]);
 
   readonly userIsManager$ = this.auth.userIsManager$;
+
+  readonly EventTableColumn = EventTableColumn;
+  readonly eventColumns: EventTableColumn[] = [
+    EventTableColumn.StartTime,
+    EventTableColumn.EndTime,
+  ];
 
   private readonly unsubscribe$ = new Subject<void>();
 
@@ -23,12 +37,16 @@ export class CourtDetailComponent implements OnInit, OnDestroy {
     private readonly router: Router,
     private readonly auth: AuthService,
     private readonly courtService: CourtService,
+    private readonly eventService: EventService,
   ) {}
 
   ngOnInit(): void {
     this.route.params.pipe(takeUntil(this.unsubscribe$)).subscribe(({ id }) => {
       this.courtService.getCourtById(id);
       this.displayedCourt$ = this.courtService.singleCourt$(id);
+
+      this.eventService.getCourtEvents(id);
+      this.displayedEvents$ = this.eventService.courtEvents$(id);
     });
   }
 
