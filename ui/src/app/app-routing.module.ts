aea0e8850331c8afb1c0b7ba49a457f1bb4b5d1a
Pavel Tobias <pavel.tobias@flowup.cz>
2021-05-20 02:25:55 +0200

    Guard routes from unauthorized activation

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LandingComponent } from './views/landing/landing.component';
 import { AboutComponent } from './views/main/about/about.component';
 import { CourtDetailComponent } from './views/main/court-detail/court-detail.component';
@@ -9,10 +10,15 @@ import {LessonDetailComponent} from "./views/main/lesson-detail/lesson-detail.co
 import {TournamentDetailComponent} from "./views/main/tournament-detail/tournament-detail.component";
 
 const routes: Routes = [
-  { path: 'landing', component: LandingComponent },
+  {
+    path: 'landing',
+    component: LandingComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'main',
     component: MainComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'about', component: AboutComponent },
---
+++ ui/src/app/guards/auth.guard.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+type ValidationResult =
+  | {
+      canActivate: true;
+    }
+  | {
+      canActivate: false;
+      redirectTo: string;
+    };
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly auth: AuthService,
+    private readonly router: Router,
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<boolean> {
+    return this.auth.loggedIn$.pipe(
+      map((loggedIn): ValidationResult => {
+        if (loggedIn && route.url[0].path === 'landing') {
+          return { canActivate: false, redirectTo: '/main' };
+        }
+
+        if (!loggedIn && route.url[0].path === 'main') {
+          return { canActivate: false, redirectTo: '/landing' };
+        }
+
+        return { canActivate: true };
+      }),
+      tap(result => {
+        if (!result.canActivate) {
+          this.router.navigateByUrl(result.redirectTo);
+        }
+      }),
+      map(({ canActivate }) => canActivate),
+    );
+  }
+}
