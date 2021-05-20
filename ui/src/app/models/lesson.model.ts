7f5c4d8fed62f1e4421b67e8ac351f8fed3e5828
Fuyunori <skywingsfs@gmail.com>
2021-05-20 16:52:48 +0200

    fixed model types back to type alias

--- ui/src/app/models/lesson.model.ts
+++ ui/src/app/models/lesson.model.ts
@@ -7,18 +7,11 @@ export enum Level {
   Advanced = 'ADVANCED',
 }
 
-export class Lesson extends Event {
-  constructor(
-    public id: number,
-    public start: Date,
-    public end: Date,
-    public capacity: number,
-    public level: Level,
-    public teachers: Set<User>,
-    public students: Set<User>,
-  ) {
-    super(id, start, end);
-  }
+export type Lesson = Event & {
+  capacity: number,
+  level: Level,
+  teachers: Set<User>,
+  students: Set<User>,
 }
 
 export type UnknownLesson = Omit<Lesson, 'id' | 'teachers' | 'students'>;
--- ui/src/app/models/tournament.model.ts
+++ ui/src/app/models/tournament.model.ts
@@ -1,18 +1,11 @@
 import { Event } from './event.model';
 import { Ranking } from './ranking.model';
 
-export class Tournament extends Event {
-  constructor(
-    public id: number,
-    public start: Date,
-    public end: Date,
-    public name: string,
-    public capacity: number,
-    public prize: number,
-    public rankings: Set<Ranking>,
-  ) {
-    super(id, start, end);
-  }
+export type Tournament = Event & {
+  name: string,
+  capacity: number,
+  prize: number,
+  rankings: Set<Ranking>,
 }
 
 export type UnknownTournament = Omit<Tournament, 'id' | 'rankings'>;
