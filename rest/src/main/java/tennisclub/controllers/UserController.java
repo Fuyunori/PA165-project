d0640fc63dbf9687482efb4d4121cd4b0dcc0c33
Pavel Tobias <pavel.tobias@flowup.cz>
2021-05-21 21:32:53 +0200

    Configure and document building the UI and serving it by Tomcat

--- rest/src/main/java/tennisclub/controllers/UserController.java
+++ rest/src/main/java/tennisclub/controllers/UserController.java
@@ -18,7 +18,7 @@ import java.util.List;
  */
 @CrossOrigin
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/rest/users")
 public class UserController {
 
     private final UserFacade userFacade;
