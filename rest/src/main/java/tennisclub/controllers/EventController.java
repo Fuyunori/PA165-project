cb65fe853229ad31349a72cda73fe75e107f22e7
Miroslav Demek <xdemek@fi.muni.cz>
2021-05-23 10:40:08 +0200

    Secure Event controller

--- rest/src/main/java/tennisclub/controllers/EventController.java
+++ rest/src/main/java/tennisclub/controllers/EventController.java
@@ -5,7 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tennisclub.dto.event.EventRescheduleDTO;
 import tennisclub.dto.event.EventWithCourtDTO;
+import tennisclub.enums.Role;
 import tennisclub.facade.EventFacade;
+import tennisclub.service.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,24 +21,31 @@ import java.util.List;
 public class EventController {
 
     private final EventFacade eventFacade;
+    private final UserService userService;
 
     @Autowired
-    public EventController(EventFacade eventFacade) {
+    public EventController(EventFacade eventFacade, UserService userService) {
         this.eventFacade = eventFacade;
+        this.userService = userService;
     }
 
     @GetMapping()
-    public ResponseEntity<List<EventWithCourtDTO>> getEvents() {
+    public ResponseEntity<List<EventWithCourtDTO>> getEvents(@RequestHeader("Authorization") String jwt) {
+        userService.verifyRole(jwt, Role.USER);
         return ResponseEntity.ok(eventFacade.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EventWithCourtDTO> getEventById(@PathVariable Long id) {
+    public ResponseEntity<EventWithCourtDTO> getEventById(@PathVariable Long id, @RequestHeader("Authorization") String jwt) {
+        userService.verifyRole(jwt, Role.USER);
         return ResponseEntity.ok(eventFacade.findById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EventWithCourtDTO> reschedule(@PathVariable Long id, @Valid @RequestBody EventRescheduleDTO rescheduleDTO) {
+    public ResponseEntity<EventWithCourtDTO> reschedule(@PathVariable Long id,
+                                                        @Valid @RequestBody EventRescheduleDTO rescheduleDTO,
+                                                        @RequestHeader("Authorization") String jwt) {
+        userService.verifyRole(jwt, Role.USER);
         return ResponseEntity.ok(eventFacade.reschedule(id, rescheduleDTO));
     }
 }
