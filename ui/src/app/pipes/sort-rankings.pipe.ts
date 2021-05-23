82f73556f4be8babc82c794b19a0b8e782cd3b56
Fuyunori <skywingsfs@gmail.com>
2021-05-23 18:49:57 +0200

    added sorting of rankings

---
+++ ui/src/app/pipes/sort-rankings.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Ranking} from "../models/ranking.model";
+
+@Pipe({
+  name: 'sortRankings'
+})
+export class SortRankingsPipe implements PipeTransform {
+
+  transform(rankings: Ranking[]): Ranking[] {
+    return rankings.sort((a: Ranking, b: Ranking) =>{
+      return a.playerPlacement - b.playerPlacement;
+    });
+  }
+
+}
