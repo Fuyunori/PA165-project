644db9df803a339ac4cbe21cee9ca415c8c5d247
Miroslav Demek <xdemek@fi.muni.cz>
2021-05-23 23:34:01 +0200

    Add response entity to user controller

--- rest/src/main/java/tennisclub/controllers/UserController.java
+++ rest/src/main/java/tennisclub/controllers/UserController.java
@@ -1,7 +1,8 @@
 package tennisclub.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tennisclub.dto.user.UserAuthDTO;
 import tennisclub.dto.user.UserFullDTO;
@@ -11,8 +12,6 @@ import tennisclub.facade.UserFacade;
 import tennisclub.service.UserService;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -35,45 +34,47 @@ public class UserController {
     }
 
     @PostMapping
-    public final UserFullDTO register(@Valid @RequestBody UserAuthDTO userAuthDTO) {
-        return userFacade.register(userAuthDTO);
+    public final ResponseEntity<UserFullDTO> register(@Valid @RequestBody UserAuthDTO userAuthDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userFacade.register(userAuthDTO));
     }
 
     @PostMapping("/login")
-    public final String authenticate(@Valid @RequestBody UserAuthDTO userAuthDTO) {
-        return userFacade.authenticate(userAuthDTO);
+    public final ResponseEntity<String> authenticate(@Valid @RequestBody UserAuthDTO userAuthDTO) {
+        return ResponseEntity.ok(userFacade.authenticate(userAuthDTO));
     }
 
     @GetMapping
-    public final List<UserFullDTO> findAllUsers(@RequestHeader(value = "Authorization", required = false) String jwt, @RequestParam(value = "username", required = false) String username) {
+    public final ResponseEntity<List<UserFullDTO>> findAllUsers(@RequestHeader(value = "Authorization", required = false) String jwt,
+                                                                @RequestParam(value = "username", required = false) String username) {
         if(username != null){
             userService.verifyRole(jwt, Role.USER);
-            return Collections.singletonList(userFacade.findUserByUsername(username));
+            return ResponseEntity.ok(Collections.singletonList(userFacade.findUserByUsername(username)));
         }
         userService.verifyRole(jwt, Role.MANAGER);
-        return userFacade.findAllUsers();
+        return ResponseEntity.ok(userFacade.findAllUsers());
     }
 
     @GetMapping("/{id}")
-    public final UserFullDTO findUserById(@RequestHeader(value = "Authorization", required = false) String jwt, @PathVariable Long id) {
+    public final ResponseEntity<UserFullDTO> findUserById(@RequestHeader(value = "Authorization", required = false) String jwt, @PathVariable Long id) {
         userService.verifyUserOrManager(jwt, id);
-        return userFacade.findUserById(id);
+        return ResponseEntity.ok(userFacade.findUserById(id));
     }
 
     @PutMapping("/{id}")
-    public final UserFullDTO updateUser(
+    public final ResponseEntity<UserFullDTO> updateUser(
             @RequestHeader(value = "Authorization", required = false) String jwt,
             @PathVariable Long id,
             @Valid @RequestBody UserUpdateDTO userUpdateDTO
     ) {
         userService.verifyUserOrManager(jwt, id);
-        return userFacade.updateUser(id, userUpdateDTO);
+        return ResponseEntity.ok(userFacade.updateUser(id, userUpdateDTO));
     }
 
     @DeleteMapping("/{id}")
-    public final void removeUser(@RequestHeader(value = "Authorization", required = false) String jwt, @PathVariable Long id) {
+    public final ResponseEntity<Void> removeUser(@RequestHeader(value = "Authorization", required = false) String jwt, @PathVariable Long id) {
         userService.verifyUserOrManager(jwt, id);
         userFacade.removeUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
